e8d8945badd5340c6e88394dcc94836845051f0c
aptend <crescentwhale@hotmail.com>
2019-06-23 22:47:25 +0800

    659, 1011

---
+++ 1011 - Capacity To Ship Packages Within D Days/1011_capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,35 @@
+from leeyzer import Solution, solution
+
+
+class Q1011(Solution):
+    @solution
+    def shipWithinDays(self, weights, D):
+        def days_needed(cap):
+            k = 1
+            this_ship_weight = 0
+            for x in weights:
+                this_ship_weight += x
+                if this_ship_weight > cap:
+                    this_ship_weight = x
+                    k += 1
+            return k
+        lo, hi = max(weights), sum(weights)
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if days_needed(mid) <= D:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return lo
+
+
+def main():
+    q = Q1011()
+    q.add_args([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5) # 15
+    q.add_args([3, 2, 2, 4, 1, 4], 3) # 6
+    q.add_args([1, 2, 3, 1, 1], 4) # 3
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 658 - Find K Closest Elements/658_find-k-closest-elements.py
@@ -0,0 +1,54 @@
+from leeyzer import Solution, solution
+
+
+class Q658(Solution):
+    @solution
+    def findClosestElements(self, arr, k, x):
+        lo, hi = 0, len(arr)-1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if arr[mid] >= x:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        lower_bound = lo
+
+        lo, hi = 0, len(arr)-1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if arr[mid] > x:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        higher_bound = lo
+
+        waiting = k - (higher_bound - lower_bound)
+        if higher_bound == len(arr):
+            return arr[-k:]
+        if lower_bound == 0 or waiting < 0:
+            return arr[:k]
+
+        i, j = lower_bound-1, higher_bound
+        for _ in range(waiting):
+            if i < 0:
+                j += 1
+            elif j >= len(arr):
+                i -= 1
+            elif x - arr[i] <= arr[j] - x:
+                i -= 1
+            else:
+                j += 1
+        return arr[i+1:j]
+
+
+def main():
+    q = Q658()
+    q.add_args([1, 2, 3, 4, 5], 4, 3)
+    q.add_args([1, 2, 3, 4, 5], 4, -1)
+    q.add_args([1, 2, 3, 4, 5], 4, 15)
+    q.add_args([1, 2, 2, 4, 5], 2, 4)
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
