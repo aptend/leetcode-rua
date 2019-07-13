c3c9fa7cea0ac14a7b550d553f46da6901d34f46
aptend <crescentwhale@hotmail.com>
2019-07-13 22:27:39 +0800

    740

---
+++ 740 - Delete and Earn/740_delete-and-earn.py
@@ -0,0 +1,45 @@
+from leeyzer import Solution, solution
+
+
+class Q740(Solution):
+    @solution
+    def deleteAndEarn(self, nums):
+        # 108ms
+        vals = [0] * 10001
+        for n in nums:
+            vals[n] += n
+        dp1 = dp2 = 0
+        for i in range(len(vals)):
+            tmp = max(dp2+vals[i], dp1)
+            dp2 = dp1
+            dp1 = tmp
+        return dp1
+    
+    @solution
+    def delete_earn(self, nums):
+        # 40ms
+        if not nums:
+            return 0
+        min_, max_ = min(nums), max(nums)
+        vals = [0] * (max_-min_+1)
+        for n in nums:
+            vals[n-min_] += n
+        dp1 = dp2 = 0
+        for i in range(len(vals)):
+            tmp = max(dp2+vals[i], dp1)
+            dp2 = dp1
+            dp1 = tmp
+        return dp1
+    
+
+
+
+def main():
+    q = Q740()
+    q.add_args([3, 4, 2])  # 6
+    q.add_args([2, 2, 3, 3, 3, 4])  # 9
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
