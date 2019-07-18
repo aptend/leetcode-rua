d2593b9913f353a58daba1f83162f11d69423a71
aptend <crescentwhale@hotmail.com>
2019-07-18 17:09:03 +0800

    127 996

---
+++ 127 - Word Ladder/127_word-ladder.py
@@ -0,0 +1,72 @@
+from leeyzer import Solution, solution
+from collections import defaultdict, deque
+
+
+class Q127(Solution):
+    @solution
+    def ladderLength(self, beginWord, endWord, wordList):
+        if endWord not in wordList:
+            return 0
+        transform = defaultdict(set)
+        L = len(beginWord)
+        for word in wordList:
+            for i in range(L):
+                transform[word[:i]+'*'+word[i+1:]].add(word)
+        queue = deque()
+        queue.append(beginWord)
+        seen = set()
+        cnt = 0
+        while queue:
+            cnt += 1
+            for _ in range(len(queue)):
+                tile = queue.popleft()
+                for i in range(L):
+                    candidates = transform[tile[:i]+'*'+tile[i+1:]]
+                    if endWord in candidates:
+                        return cnt+1
+                    queue.extend(candidates - seen)
+                    seen |= candidates
+        return 0
+
+    @solution
+    def ladder_bibfs(self, beginWord, endWord, wordList):
+        if endWord not in wordList:
+            return 0
+        transform = defaultdict(set)
+        L = len(beginWord)
+        for word in wordList:
+            for i in range(L):
+                transform[word[:i]+'*'+word[i+1:]].add(word)
+
+        s1, s2, seen = {beginWord}, {endWord}, set()
+        cnt = 0
+        while s1 and s2:
+            cnt += 1
+            if len(s1) > len(s2):
+                s1, s2 = s2, s1
+            nxt_s = set()
+            for tile in s1:
+                for i in range(L):
+                    candidates = transform[tile[:i]+'*'+tile[i+1:]]
+                    if len(candidates) == 0:
+                        continue
+                    if len(candidates & s2) > 0:
+                        return cnt+1
+                    nxt_s |= candidates - seen
+                    seen |= candidates
+            s1 = nxt_s
+        return 0
+
+
+def main():
+    q = Q127()
+    q.add_args("hit", "cog",
+               ["hot", "dot", "dog", "lot", "log", "cog"])  # 5
+    q.add_args("hit", "mot",
+               ["hie", "dot", "mit", "mot", "dog", "lot", "log", "cog"])  # 3
+    q.add_args("hit", "cog",
+               ["hot", "dot", "dog", "lot", "log"])  # 0
+    q.run()
+
+if __name__ == '__main__':
+    main()
---
+++ 996 - Number of Squareful Arrays/996_number-of-squareful-arrays.py
@@ -0,0 +1,45 @@
+from leeyzer import Solution, solution
+import math
+
+class Q996(Solution):
+    @solution
+    def numSquarefulPerms(self, A):
+        """20ms beats 75.70%
+        """
+        ans = [0]
+        used = [False] * len(A)
+        A = sorted(A)
+        self.dfs(A, 0, used, None, ans)
+        return ans[0]
+
+    def is_squareful(self, x, y):
+        s = int(math.sqrt(x+y))
+        return s*s == x+y
+
+    def dfs(self, A, depth, used, prev, ans):
+        if depth == len(A):
+            ans[0] += 1
+            return
+        prev_tried = -1
+        for i in range(len(A)):
+            if used[i] or A[i] == prev_tried:
+                continue
+            if prev is not None and not self.is_squareful(prev, A[i]):
+                continue
+            used[i] = True
+            prev_tried = A[i]
+            self.dfs(A, depth+1, used, A[i], ans)
+            used[i] = False
+
+
+def main():
+    q = Q996()
+    q.add_args([3])  # 1
+    q.add_args([0, 3])  # 0
+    q.add_args([1, 17, 8])
+    q.add_args([2, 2, 7])
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
