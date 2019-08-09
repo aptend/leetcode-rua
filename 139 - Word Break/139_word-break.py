fe73959c71ce991fc4e193c97a026a9b53ad9e9a
aptend <crescentwhale@hotmail.com>
2019-08-09 23:26:10 +0800

    word break 139 140

---
+++ 139 - Word Break/139_word-break.py
@@ -0,0 +1,47 @@
+from leeyzer import Solution, solution
+
+
+class Q139(Solution):
+    @solution
+    def wordBreak(self, s, wordDict):
+        # review search method
+        # 28ms 63.07% / TLE w/o memo
+        word_dict = set(wordDict)
+        memo = {}
+        def dfs(i, formed):
+            if i >= len(s):
+                return formed == ''
+            if i in memo:
+                return memo[i]
+            for j in range(i, len(s)):
+                formed += s[j]
+                if formed in word_dict and dfs(j+1, ''):
+                    return True
+            memo[i] = False
+            return False
+        return dfs(0, '')
+
+    @solution
+    def word_break_dp(self, s, wordDict):
+        # 24ms 81.60% / 32ms
+        N = len(s)
+        word_dict = set(wordDict)
+        dp = [False for _ in range(N+1)]
+        dp[0] = True
+        for i in range(1, N+1):
+            for j in range(i-1, -1, -1):
+                if s[j:i] in word_dict and dp[j]:
+                    dp[i] = True
+                    break
+        return dp[N]
+
+def main():
+    q = Q139()
+    q.add_args('leetcode', ['leet', 'code'])
+    q.add_args('apppenapp', ['app', 'pen'])
+    q.add_args('catsandog', ["cats", "dog", "sand", "and", "cat"])
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 140 - Word Break II/140_word-break-ii.py
@@ -0,0 +1,36 @@
+from leeyzer import Solution, solution
+
+
+class Q140(Solution):
+    @solution
+    def wordBreak(self, s, wordDict):
+        # 32ms 80.33%
+        word_dict = set(wordDict)
+        memo = {len(s): []}
+        def sub_problem(i):
+            if i in memo:
+                return memo[i]
+            solns = []
+            if s[i:] in word_dict:
+                solns.append(s[i:])
+            for j in range(i, len(s)):
+                word = s[i:j+1]
+                if word in word_dict:
+                    solns.extend(
+                        word + ' ' + item for item in sub_problem(j+1))
+            memo[i] = solns
+            return solns
+        return sub_problem(0)
+
+
+def main():
+    q = Q140()
+    q.add_args('aaaaaaa', ['a', 'aa', 'aaaa'])
+    q.add_args('catsanddog', ['cat', 'cats', 'and', 'sand', 'dog'])
+    q.add_args('catsandog', ['cat', 'cats', 'and', 'sand', 'dog'])
+    q.add_args('pineapplepenapple', ["apple", "pen", "applepen", "pine", "pineapple"])
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
