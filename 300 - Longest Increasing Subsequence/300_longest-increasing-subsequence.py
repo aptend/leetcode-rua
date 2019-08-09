82a4d9a0d9b1681615be470d072540e725f345c2
aptend <crescentwhale@hotmail.com>
2019-08-09 22:53:47 +0800

    LIS 300 673

---
+++ 300 - Longest Increasing Subsequence/300_longest-increasing-subsequence.py
@@ -0,0 +1,37 @@
+from leeyzer import Solution, solution
+from bisect import bisect_left
+
+class Q300(Solution):
+    @solution
+    def lengthOfLIS(self, nums):
+        # O(n^2) 828ms 59.76%
+        if not nums:
+            return 0
+        N = len(nums)
+        # dp[i] means max length among all subsequences ending with nums[i]
+        dp = [1 for _ in range(N)]
+        for i in range(N):
+            for j in range(i):
+                if nums[j] < nums[i] and dp[j] >= dp[i]:
+                    dp[i] = dp[j]+1
+        return max(dp)
+
+    @solution
+    def length_of_LIS_better(self, nums):
+        pass
+        # to be continued
+
+
+
+
+def main():
+    q = Q300()
+    # q.add_args([])
+    # q.add_args([2, 2, 2])
+    # q.add_args([10, 9, 2, 5, 3, 7, 101, 18])
+    q.add_args([1, 3, 6, 7, 9, 4, 10, 5, 6])
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 673 - Number of Longest Increasing Subsequence/673_number-of-longest-increasing-subsequence.py
@@ -0,0 +1,57 @@
+from leeyzer import Solution, solution
+
+class Q673(Solution):
+    @solution
+    def findNumberOfLIS(self, nums):
+        # Python3 1476ms 5.19%
+        if not nums:
+            return 0
+        N = len(nums)
+        # dp[i] means max length of LIS ending with nums[i]
+        dp = [0 for _ in range(N)]
+        # dp_n[i] means numbers of LIS ending with nums[i]
+        dp_n = [0 for _ in range(N)]
+        for i in range(N):
+            dp[i] = 1 + max((dp[j] for j in range(i) if nums[j] < nums[i]),
+                            default=0)
+            n = sum(dp_n[j] for j in range(i) if dp[j] == dp[i]-1 and nums[j] < nums[i])
+            # n = 0 means nums[i] can't be attached to any seqs,
+            # LIS is itself alone, only one
+            dp_n[i] = n if n > 0 else 1
+        max_len = max(dp)
+        return sum(dp_n[j] for j in range(N) if dp[j] == max_len)
+
+    @solution
+    def numberOfLIS(self, nums):
+        # 712ms 77.65%
+        if not nums:
+            return 0
+        N = len(nums)
+        dp = [1 for _ in range(N)]
+        dp_n = [0 for _ in range(N)]
+        for i in range(N):
+            max_len, count = 1, 0
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    if dp[j] >= max_len:
+                        max_len = dp[j] + 1
+                        count = 0
+                    if dp[j] == max_len - 1:
+                        count += dp_n[j]
+            dp[i] = max_len
+            dp_n[i] = count if count > 0 else 1
+        max_len = max(dp)
+        return sum(dp_n[j] for j in range(N) if dp[j] == max_len)
+
+
+
+def main():
+    q = Q673()
+    q.add_args([1, 3, 5, 4, 7]) # 2
+    q.add_args([1, 2, 4, 3, 5, 4, 7, 2]) # 3
+    q.add_args([2, 2, 3, 2]) # 2
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
