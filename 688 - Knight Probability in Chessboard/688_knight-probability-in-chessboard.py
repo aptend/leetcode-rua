e937bedfa991923c7de16384e18e1348633897a1
aptend <crescentwhale@hotmail.com>
2019-08-15 23:19:08 +0800

    knight dp 688 935

---
+++ 688 - Knight Probability in Chessboard/688_knight-probability-in-chessboard.py
@@ -0,0 +1,44 @@
+from leeyzer import Solution, solution
+
+class Q688(Solution):
+    @solution
+    def knightProbability(self, N, K, r, c):
+        # 128ms 93.33%
+        directions = [
+            (1, 2), (1, -2), (-1, 2), (-1, -2),
+            (2, 1), (2, -1), (-2, 1), (-2, -1),
+        ]
+        dp = [[0]*N for _ in range(N)]
+        cur_dp = [[0]*N for _ in range(N)]
+        dp[r][c] = 1
+        for _ in range(K):
+            print(cur_dp)
+            for i in range(N):
+                for j in range(N):
+                    if dp[i][j] == 0:
+                        continue
+                    for di, dj in directions:
+                        ni, nj = i+di, j+dj
+                        if 0 <= ni < N and 0 <= nj < N:
+                            cur_dp[ni][nj] += dp[i][j]
+            cur_dp, dp = dp, cur_dp
+            print(dp)
+            for row in cur_dp:
+                for i in range(N):
+                    row[i] = 0
+        cnt = 0
+        for row in dp:
+            for x in row:
+                cnt += x
+        return cnt / (8**K)
+
+
+
+def main():
+    q = Q688()
+    q.add_args(3, 2, 0, 0)
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 935 - Knight Dialer/935_knight-dialer.py
@@ -0,0 +1,47 @@
+from leeyzer import Solution, solution
+
+
+class Q935(Solution):
+    @solution
+    def knightDialer(self, N):
+        # 1004ms 47.75%
+        # can be optmized by dismissing `move_map`
+        # variables updating can be hard-coded in the loop
+        # another point:
+        # mod operation can be arbitrarily inserted into a linear computing process 
+        # without affecting the final result
+        move_map = [
+            (4, 6),
+            (6, 8),
+            (7, 9),
+            (4, 8),
+            (3, 9, 0),
+            (),
+            (1, 7, 0),
+            (2, 6),
+            (1, 3),
+            (2, 4)
+        ]
+        kmod = 1e9 + 7
+        dp = [1] * 10
+        cur_dp = [0] * 10
+        for _ in range(N-1):
+            for i in range(10):
+                for dest in move_map[i]:
+                    cur_dp[dest] = (cur_dp[dest] + dp[i]) % kmod
+            cur_dp, dp = dp, cur_dp
+            for j in range(10):
+                cur_dp[j] = 0
+        return int(sum(dp) % kmod)
+
+
+def main():
+    q = Q935()
+    q.add_args(1)
+    q.add_args(2)
+    q.add_args(3)
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
