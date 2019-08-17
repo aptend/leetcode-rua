6386427faec6a97d84315026a55361984848b6ae
aptend <crescentwhale@hotmail.com>
2019-08-17 18:37:33 +0800

    random pick 209

---
+++ 209 - Minimum Size Subarray Sum/209_minimum-size-subarray-sum.py
@@ -0,0 +1,36 @@
+from leeyzer import Solution, solution
+
+
+class Q209(Solution):
+    @solution
+    def minSubArrayLen(self, s, nums):
+        # 56ms 82.79%
+        N = len(nums)
+        i, j = 0, 0
+        ans = N+1
+        subsum = 0
+        while True:
+            while j < N and subsum < s:
+                subsum += nums[j]
+                j += 1
+            while i < N and subsum >= s:
+                subsum -= nums[i]
+                i += 1
+            ans = min(ans, j-i+1)
+            if j == N:
+                break
+        return ans if ans <= N else 0
+
+
+def main():
+    q = Q209()
+    q.add_args(7, [])
+    q.add_args(7, [2, 3, 1, 2, 4, 3])
+    q.add_args(13, [2, 3, 1, 2, 4, 3])
+    q.add_args(15, [2, 3, 1, 2, 4, 3])
+    q.add_args(20, [2, 3, 1, 2, 4, 3])
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
