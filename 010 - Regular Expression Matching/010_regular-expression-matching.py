ee3a12faff6d5fdb0c039fd3c8e8390d53036fbc
aptend <crescentwhale@hotmail.com>
2019-08-23 14:30:56 +0800

    2 strings dp 10 97

---
+++ 010 - Regular Expression Matching/010_regular-expression-matching.py
@@ -0,0 +1,47 @@
+from leeyzer import Solution, solution
+
+
+class Q010(Solution):
+    @solution
+    def isMatch(self, s, p):
+        # 16ms 99.82%
+        m, n = len(s), len(p)
+        memo = [[None] * (n+1) for _ in range(m+1)]
+
+        def match(i, j):
+            # when pattern is depleted, we see if str is also depleted
+            if j == n:
+                return i == m
+            if memo[i][j] is not None:
+                return memo[i][j]
+            s_avail = i < m
+            # when s is depleted while p is not, head_match is False
+            head_match = s_avail and (s[i] == p[j] or p[j] == '.')
+            if j < n - 1 and p[j+1] == '*':
+                # "*" pattern, which can handle empty s
+                ans = match(i, j+2)
+                if head_match:
+                    # when head matchs, use '*' once or more
+                    ans = ans or match(i+1, j) or match(i+1, j+2)
+            else:
+                # '.' pattern alone or no pattern, skip if we can
+                ans = head_match and match(i+1, j+1)
+
+            memo[i][j] = ans
+            return ans
+        return match(0, 0)
+
+
+def main():
+    q = Q010()
+    q.add_args('', '.')
+    q.add_args('aa', 'a')
+    q.add_args('aa', 'a*')
+    q.add_args('ab', '.*')
+    q.add_args('aab', 'c*a*b')
+    q.add_args(s="mississippi", p="mis*is*p*.")
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 097 - Interleaving String/097_interleaving-string.py
@@ -0,0 +1,57 @@
+from leeyzer import Solution, solution
+
+
+class Q097(Solution):
+    @solution
+    def isInterleave(self, s1, s2, s3):
+        # 16ms 95.04%
+        m, n = len(s1), len(s2)
+        if m + n != len(s3):
+            return False
+        memo = [[None] * (n+1) for _ in range(m+1)]
+
+        def is_intv(i, j):
+            if memo[i][j] is not None:
+                return memo[i][j]
+            s3_idx = i + j
+            if i == 0:
+                ans = s3[:s3_idx] == s2[:j]
+            elif j == 0:
+                ans = s3[:s3_idx] == s1[:i]
+            else:
+                ans = ((s1[i-1] == s3[s3_idx-1] and is_intv(i-1, j)) or
+                       (s2[j-1] == s3[s3_idx-1] and is_intv(i, j-1)))
+            memo[i][j] = ans
+            return ans
+        return is_intv(m, n)
+
+    @solution
+    def is_interleave(self, s1, s2, s3):
+        # 32ms
+        m, n = len(s1), len(s2)
+        if m + n != len(s3):
+            return False
+        dp = [[False] * (n+1) for _ in range(m+1)]
+        for i in range(m+1):
+            dp[i][0] = s1[:i] == s3[:i]
+        for j in range(n+1):
+            dp[0][j] = s2[:j] == s3[:j]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp[i][j] = ((s1[i-1] == s3[i+j-1] and dp[i-1][j]) or
+                            (s2[j-1] == s3[i+j-1] and dp[i][j-1]))
+        return dp[m][n]
+
+
+def main():
+    q = Q097()
+    q.add_args('', '', '')
+    q.add_args('a', '', 'c')
+    q.add_args('a', '', 'a')
+    q.add_args('aabcc', 'dbbca', 'aadbbcbcac')
+    q.add_args(s1="aabcc", s2="dbbca", s3="aadbbbaccc")
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
