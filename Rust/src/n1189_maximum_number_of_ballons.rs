623ce533344ec553e8bd349a79b8cfb625a1a277
aptend <crescentwhale@hotmail.com>
2019-09-15 22:10:58 +0800

    rustfmt

--- Rust/src/n1189_maximum_number_of_ballons.rs
+++ Rust/src/n1189_maximum_number_of_ballons.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
 use std::cmp::min;
+use std::collections::HashMap;
 
 pub fn max_number_of_balloons(text: String) -> i32 {
     let mut counter: HashMap<char, i32> = HashMap::new();
@@ -7,10 +7,10 @@ pub fn max_number_of_balloons(text: String) -> i32 {
         *counter.entry(ch).or_insert(0) += 1
     }
     let mut ans = text.len() as i32;
-    for ch in "ban".chars(){
+    for ch in "ban".chars() {
         ans = min(ans, *counter.get(&ch).unwrap_or(&0));
-    }   
-    for ch in "lo".chars(){
+    }
+    for ch in "lo".chars() {
         ans = min(ans, *counter.get(&ch).unwrap_or(&0) / 2);
     }
     ans
--- Rust/src/n1190_reverse_substrings_between_each_pair_of_parentheses.rs
+++ Rust/src/n1190_reverse_substrings_between_each_pair_of_parentheses.rs
@@ -6,13 +6,12 @@ pub fn reverse_parentheses(s: String) -> String {
             ')' => {
                 let inner = stack.pop().unwrap();
                 stack.last_mut().unwrap().extend(inner.iter().rev());
-            },
+            }
             _ => stack.last_mut().unwrap().push(ch),
         }
     }
     stack.last().unwrap().iter().collect::<String>()
 }
-
 
 #[test]
 fn test_1190() {
@@ -20,5 +19,8 @@ fn test_1190() {
     assert_eq!("abc", reverse_parentheses("a()((bc))".to_owned()));
     assert_eq!("iloveu", reverse_parentheses("(u(love)i)".to_owned()));
     assert_eq!("leetcode", reverse_parentheses("(ed(et(oc))el)".to_owned()));
-    assert_eq!("apmnolkjihgfedcbq", reverse_parentheses("a(bcdefghijkl(mno)p)q".to_owned()));
+    assert_eq!(
+        "apmnolkjihgfedcbq",
+        reverse_parentheses("a(bcdefghijkl(mno)p)q".to_owned())
+    );
 }
