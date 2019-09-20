b4ec0d9cf1e858f0b5cb8867928de56ec99960b7
aptend <crescentwhale@hotmail.com>
2019-09-20 22:05:47 +0800

    random 830

---
+++ Python/830 - Positions of Large Groups/830_positions-of-large-groups.py
@@ -0,0 +1,29 @@
+from leeyzer import Solution, solution
+
+
+class Q830(Solution):
+    @solution
+    def largeGroupPositions(self, S):
+        pass
+        # 48ms 50.57%
+        N = len(S)
+        ans = []
+        start = i = 0
+        for i in range(N-1):
+            if S[i] != S[i+1]:
+                if i - start >= 2:
+                    ans.append([start, i])
+                start = i + 1
+        if N - 1 - start >= 2:
+            ans.append([start, N-1])
+        return ans
+
+
+def main():
+    q = Q830()
+    q.add_args('abbxxxxzzy')
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
