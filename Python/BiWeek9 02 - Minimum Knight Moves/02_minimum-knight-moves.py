79649144f1cfe512f14354f2b4e344e8d2566a28
aptend <crescentwhale@hotmail.com>
2019-09-26 23:24:01 +0800

    BiWeek9 2 & 3

---
+++ Python/BiWeek9 02 - Minimum Knight Moves/02_minimum-knight-moves.py
@@ -0,0 +1,48 @@
+from leeyzer import Solution, solution
+
+from collections import deque
+class Q02(Solution):
+    @solution
+    def minKnightMoves(self, x, y):
+        # 480ms 66.67%
+        if x == 0 and y == 0:
+            return 0
+        dirs = [
+            (1, 2), (1, -2), (2, 1), (2, -1),
+            (-1, 2), (-1, -2), (-2, 1), (-2, -1)
+        ]
+        q = deque()
+        q.append((0, 0))
+        # we just need to check a quarter of the whole chess board
+        x, y = abs(x), abs(y)
+        terminal = (x, y)
+        seen = set()
+        cnt = 0
+        while q:
+            cnt += 1
+            for _ in range(len(q)):
+                cx, cy = q.popleft()
+                for di, dj in dirs:
+                    nx, ny = cx + di, cy + dj
+                    np = (nx, ny)
+                    if np == terminal:
+                        return cnt
+                    # if x, y is accessable, we know that all path points are within 
+                    # the rectangle [0, 0, x+2, y+2]
+                    if np not in seen and 0 <= nx < x + 2 and 0 <= ny < y + 2:
+                        seen.add(np)
+                        q.append(np)
+        return -1
+
+
+def main():
+    q = Q02()
+    q.add_args(0, 0)
+    q.add_args(2, 1)
+    q.add_args(5, 5)
+    q.add_args(11, 248) # 125
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Python/BiWeek9 03 - Find Smallest Common Element in All Rows/03_find-smallest-common-element-in-all-rows.py
@@ -0,0 +1,26 @@
+from leeyzer import Solution, solution
+
+from bisect import bisect_left
+class Q02(Solution):
+    @solution
+    def smallestCommonElement(self, mat):
+        for x in mat[0]:
+            for row in mat[1:]:
+                idx = bisect_left(row, x)
+                if idx == len(row) or row[idx] != x:
+                    break
+            else:
+                return x
+        return -1
+
+
+
+def main():
+    q = Q02()
+    q.add_args([[1, 2, 3, 4, 5], [2, 4, 5, 8, 10],
+                [3, 5, 7, 9, 11], [1, 3, 5, 7, 9]])
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
