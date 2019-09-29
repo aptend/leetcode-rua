60639326a8a7b4bde8b9e0d483c1efc632084e6e
aptend <crescentwhale@hotmail.com>
2019-09-29 23:54:55 +0800

    W156 p1 1207

---
+++ Python/1207 - Unique Number of Occurrences/1207_unique-number-of-occurrences.py
@@ -0,0 +1,20 @@
+from leeyzer import Solution, solution
+
+from collections import Counter
+
+class Q1207(Solution):
+    @solution
+    def uniqueOccurrences(self, arr):
+        # 48ms
+        cnt = Counter(arr)
+        return len(set(cnt.values())) == len(cnt)
+
+
+def main():
+    q = Q1207()
+    q.add_args([1, 2, 2, 1, 1, 3])
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
