231107e7883b35fb9e9b24d0b4d1d4efad4b3e87
aptend <crescentwhale@hotmail.com>
2019-10-05 12:39:18 +0800

    991 math

---
+++ Python/991 - Broken Calculator/991_broken-calculator.py
@@ -0,0 +1,19 @@
+from leeyzer import Solution, solution
+
+
+class Q991(Solution):
+    @solution
+    def brokenCalc(self, X, Y):
+        if X >= Y:
+            return X - Y
+        else:
+            return 1 + self.brokenCalc(X, Y // 2 if Y % 2 == 0 else Y + 1)
+
+def main():
+    q = Q991()
+    q.add_args(2, 3)
+    q.run()
+
+
+if __name__ == "__main__":
+    main()
