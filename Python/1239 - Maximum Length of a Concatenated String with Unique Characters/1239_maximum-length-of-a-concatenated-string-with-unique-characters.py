94a7e0b78fc544dc393542ef991526f354366e77
aptend <crescentwhale@hotmail.com>
2019-10-27 23:26:51 +0800

    contest 160 1239

---
+++ Python/1239 - Maximum Length of a Concatenated String with Unique Characters/1239_maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,31 @@
+from leeyzer import solution, Solution
+
+class Q1239(Solution):
+    @solution
+    def maxLength(self, arr):
+        # 116ms 25%
+        sets = []
+        for s in arr:
+            st = set(s)
+            if len(st) == len(s):
+                sets.append(st)
+        self.ans = 0
+        def dfs(start, cur):
+            self.ans = max(self.ans, len(cur))
+            for i in range(start, len(sets)):
+                if len(cur - sets[i]) == len(cur):
+                    cur |= sets[i]
+                    dfs(i + 1, cur)
+                    cur -= sets[i]
+        dfs(0, set())
+        return self.ans
+        
+
+
+def main():
+    q = Q1239()
+    q.add_args(['un', 'iq', 'ue'])
+    q.run()
+
+if __name__ == '__main__':
+    main()
