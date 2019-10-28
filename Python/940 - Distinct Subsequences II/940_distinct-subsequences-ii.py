cba8289aac96978183caa2af739b18a5f0a7b929
aptend <crescentwhale@hotmail.com>
2019-10-28 20:19:34 +0800

    940 ⭐

---
+++ Python/940 - Distinct Subsequences II/940_distinct-subsequences-ii.py
@@ -0,0 +1,50 @@
+from leeyzer import solution, Solution
+
+class Q940(Solution):
+    @solution
+    def distinctSubseqII(self, S):
+        # TLE 93/109
+        N = len(S)
+        # dp[i] means how many subseq ending with S[i]
+        # initialize dp with 1, because, 
+        # for 'abcc', 
+        # dp[0] has at least 'a'
+        # 'ab' for dp[1], 'abc' for dp[2], 'abcc' for dp[3]
+        dp = [1] * N
+        for i in range(N):
+            for j in range(i):
+                if S[i] != S[j]:
+                    dp[i] += dp[j]
+        return sum(dp) % (10**9 + 7)
+    
+    @solution
+    def distinct_subseq(self, S):
+        # 112ms 19.91%
+        # dp[0] means how many subseqs ending with 'a'
+        # dp[25] means how many subseqs ending with 'z'
+        dp = [0] * 26
+        for ch in S:
+            dp[ord(ch) - ord('a')] = sum(dp) + 1
+        return sum(dp) % (10**9 + 7)
+
+    @solution
+    def distinct_subseq(self, S):
+        # 112ms 19.91%
+        # dp[0] means how many subseqs ending with 'a'
+        # dp[25] means how many subseqs ending with 'z'
+        dp = 0
+        for ch in S:
+            dp[ord(ch) - ord('a')] = sum(dp) + 1
+        return sum(dp) % (10**9 + 7)
+
+
+
+def main():
+    q = Q940()
+    q.add_args('abc')
+    q.add_args('aba')
+    q.add_args('aaa')
+    q.run()
+
+if __name__ == '__main__':
+    main()
