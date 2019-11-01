ae27248638c665287982c3a543f72512ac3884fe
aptend <crescentwhale@hotmail.com>
2019-11-01 20:28:16 +0800

    23 merge k linked list

---
+++ Python/023 - Merge k Sorted Lists/023_merge-k-sorted-lists.py
@@ -0,0 +1,68 @@
+from leeyzer import solution, Solution
+
+from leeyzer.assists import LinkedListNode
+from heapq import heappop, heappush
+
+class Q023(Solution):
+
+    def merge(self, s1, s2):
+        dummy = LinkedListNode()
+        tail = dummy
+        while s1 and s2:
+            if s1.val <= s2.val:
+                tail.next = s1
+                tail = s1
+                s1 = s1.next
+            else:
+                tail.next = s2
+                tail = s2
+                s2 = s2.next
+        tail.next = s1 or s2
+        return dummy.next
+
+    @solution
+    def mergeKLists(self, lists):
+        # 124ms
+        n = len(lists)
+        while n > 1:
+            for i in range(0, n, 2):
+                if i == n - 1:
+                    lists[i//2] = lists[i]
+                else:
+                    lists[i//2] = self.merge(lists[i], lists[i+1])
+            n = (n+1) // 2
+        return lists[0] if lists else None
+    
+    @solution
+    def merge_lists(self, lists):
+        # 108ms 89.38%
+        heap = []
+        for i, l in enumerate(lists):
+            if not l:
+                continue
+            heappush(heap, (l.val, i, l))
+            lists[i] = l.next
+        dummy = tail = LinkedListNode()
+        while heap:
+            _, i, node = heappop(heap)
+            tail.next, tail = node, node
+            if lists[i]:
+                nxt = lists[i]
+                heappush(heap, (nxt.val, i, nxt))
+                lists[i] = nxt.next
+        return dummy.next
+
+
+
+
+def main():
+    q = Q023()
+    q.add_args([])
+    q.add_args([
+        LinkedListNode.make_linked_list([1, 4, 5]), 
+        LinkedListNode.make_linked_list([1, 3, 4]),
+        LinkedListNode.make_linked_list([2, 6])])
+    q.run()
+
+if __name__ == '__main__':
+    main()
