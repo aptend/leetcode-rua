e9a36c84cc87a4b15bec2514ab47ff764eb7ca22
aptend <crescentwhale@hotmail.com>
2019-11-07 20:28:29 +0800

    617

---
+++ Python/617 - Merge Two Binary Trees/617_merge-two-binary-trees.py
@@ -0,0 +1,26 @@
+from leeyzer import solution, Solution
+from leeyzer.assists import TreeContext
+
+class Q617(Solution):
+    @solution
+    def mergeTrees(self, t1, t2):
+        # 72ms 99%
+        if not t1:
+            return t2
+        if not t2:
+            return t1
+
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
+
+
+def main():
+    q = Q617()
+    q.set_context(TreeContext)
+    q.add_args([1, 3, 2, 5], [2, 1, 3, None, 4, None, 7])
+    q.run()
+
+if __name__ == '__main__':
+    main()
