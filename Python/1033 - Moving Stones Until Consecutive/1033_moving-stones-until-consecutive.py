57e24e4b54c623e59f51e43b82ec445e2b987110
aptend <crescentwhale@hotmail.com>
2019-11-08 20:35:11 +0800

    Contest 134 1033

---
+++ Python/1033 - Moving Stones Until Consecutive/1033_moving-stones-until-consecutive.py
@@ -0,0 +1,24 @@
+from leeyzer import solution, Solution
+
+class Q1033(Solution):
+    @solution
+    def numMovesStones(self, a, b, c):
+        # 20ms 100.00%
+        a, b, c = sorted([a, b, c])
+        max_s = c - a - 2
+        if a + 1 == b and b + 1 == c:
+            return [0, max_s]
+        if a + 1 == b or b + 1 == c:
+            return [1, max_s]
+        if b - a == 2 or c - b == 2:
+            return [1, max_s]
+        return [2, max_s]
+
+
+def main():
+    q = Q1033()
+    q.add_args(1, 2, 5)
+    q.run()
+
+if __name__ == '__main__':
+    main()
