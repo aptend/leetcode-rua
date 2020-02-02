0b1ba846196d780b34c1ec683a98396baff0f653
aptend <crescentwhale@hotmail.com>
2020-02-02 16:54:51 +0800

    143 reorder linked list

---
+++ Python/143 - Reorder List/143_reorder-list.py
@@ -0,0 +1,48 @@
+from leezy import solution, Solution
+from leezy.assists import LinkedListContext, LinkedListNode
+
+
+class Q143(Solution):
+    @solution
+    def reorderList(self, head):
+        # 96ms 50.22%
+        if not head:
+            return
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        half2 = LinkedListNode()
+        node = slow.next  # this point is tricky, be careful
+        slow.next = None
+        while node:
+            next_node = node.next
+            node.next = half2.next
+            half2.next = node
+            node = next_node
+        ans = LinkedListNode()
+        dummy = ans
+        half1 = head
+        half2 = half2.next
+        while half2:
+            ans.next = half1
+            half1 = half1.next
+            ans.next.next = half2
+            ans = half2
+            half2 = half2.next
+        ans.next = half1
+        return dummy.next
+
+
+def main():
+    q = Q143()
+    q.set_context(LinkedListContext)
+    q.add_case(q.case([1, 2, 3, 4]))
+    q.add_case(q.case([1, 2, 3, 4, 5]))
+    q.add_case(q.case([]))
+    q.add_case(q.case([1, 2]))
+    q.add_case(q.case([1]))
+    q.run()
+
+if __name__ == '__main__':
+    main()
