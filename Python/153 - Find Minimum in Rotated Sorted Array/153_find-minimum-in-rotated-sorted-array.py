0df0228472fa38d3bb17595be99e3630158249df
aptend <crescentwhale@hotmail.com>
2020-02-19 21:15:24 +0800

    review 153 154

--- Python/153 - Find Minimum in Rotated Sorted Array/153_find-minimum-in-rotated-sorted-array.py
+++ Python/153 - Find Minimum in Rotated Sorted Array/153_find-minimum-in-rotated-sorted-array.py
@@ -5,42 +5,42 @@ class Q153(Solution):
     @solution
     def findMin(self, nums):
         lo, hi = 0, len(nums) - 1
-        if nums[lo] < nums[hi]:  # 排除有序的情况，因为切分后是两个有序数组
+        if nums[lo] < nums[hi]:
             return nums[lo]
         while hi - lo > 1:
             mid = lo + (hi - lo) // 2
-            if nums[mid] > nums[lo]:  # 无法取到等号，等号成立时，mid=lo，已经跳出循环了
+            if nums[mid] > nums[lo]:  # no equal here, loop break before mid = lo
                 lo = mid
-            else:                     # 小于
+            else:
                 hi = mid
         return nums[hi]
 
     @solution
     def find_min(self, nums):
         lo, hi = 0, len(nums)-1
-        if nums[lo] < nums[hi]:  # 排除有序的情况，因为不存在旋转数组，实际上python的-1索引可用
+        if nums[lo] < nums[hi]:
             return nums[lo]
         while lo <= hi:
             mid = lo + (hi - lo) // 2
-            # 实际上，只要确保当前的比较顺序，并不需要边界检查
-            # 合理的输入不可能mid=len(nums)，因为如果逆序对在最后两个，那么前一次循环已经找到了
-            # mid=0可能，所以用现在这样的比较顺序，逆序对在头两个时可以先返回
+            # no duplicates in array, save us from checking boundry
+            # the inversed pair maybe the first two elements
+            # so we check mid with its right neighbor
             if nums[mid] > nums[mid+1]:
                 return nums[mid+1]
             if nums[mid-1] > nums[mid]:
                 return nums[mid]
-            # 没找到，自然应该排除mid，因为mid不是逆序对中的任何一个
             if nums[mid] >= nums[lo]:
-                lo = mid + 1
+                lo = mid + 1 # mid is not the answer
             else:
                 hi = mid - 1
+        # no duplicates, so we will find answer in the loop
 
 
 def main():
     q = Q153()
-    q.add_args([3, 4, 5, 1, 2])
-    q.add_args([1, 2, 3, 4, 5])
-    q.add_args([5, 1, 2, 3, 4])
+    q.add_case(q.case([3, 4, 5, 1, 2]).assert_equal(1))
+    q.add_case(q.case([1, 2, 3, 4, 5]).assert_equal(1))
+    q.add_case(q.case([5, 1, 2, 3, 4]).assert_equal(1))
     q.run()
 
 
--- Python/154 - Find Minimum in Rotated Sorted Array II/154_find-minimum-in-rotated-sorted-array-ii.py
+++ Python/154 - Find Minimum in Rotated Sorted Array II/154_find-minimum-in-rotated-sorted-array-ii.py
@@ -4,38 +4,67 @@ from leezy import Solution, solution
 class Q154(Solution):
     @solution
     def findMin(self, nums):
+        """this solution aims at finding the inversed pair
+
+        when the loop ends, the two digits left are the inversed pair
+        """
         lo, hi = 0, len(nums)-1
-        if nums[lo] < nums[hi]:  # 排除有序的情况，因为切分后是两个有序数组
+        if nums[lo] < nums[hi]:  # original array is sorted
             return nums[lo]
         while hi - lo > 1:
             mid = lo + (hi - lo) // 2
-            if nums[mid] > nums[lo]:  # 左侧有序，排除有序
-                lo = mid
-            elif nums[mid] < nums[lo]:  # 右侧有序，排除有序
+            if nums[mid] > nums[lo]:    # left array is sorted, exclude
+                lo = mid                # keep mid, maybe it is the minimum
+            elif nums[mid] < nums[lo]:  # right array is sorted, exclude
                 hi = mid
-            elif nums[lo] <= nums[lo+1]:
+            elif nums[lo] <= nums[lo+1]:  # move lo when nums[lo] == nums[mid]
                 lo += 1
             else:
+                # if nums[lo] > nums[lo+1], we find the answer
                 return nums[lo+1]
+
         return nums[hi]
 
     @solution
+    def find_min(self, A):
+        lo, hi = 0, len(A) - 1
+        if A[lo] < A[hi]:
+            return A[lo]
+        while hi - lo > 1:
+            m = lo + (hi - lo) // 2
+            if A[lo] == A[m]:  # handle this case explicitly
+                while lo < m and A[lo] == A[m]:
+                    lo += 1
+                if A[lo] < A[m]:
+                    return A[lo]
+                elif A[lo] > A[m]:
+                    return A[m]
+            elif A[lo] < A[m]:
+                lo = m
+            else:
+                hi = m
+        return A[hi]
+
+    @solution
     def findMin2(self, nums):
+        """we can find the inversed pair in the loop
+        """
         lo, hi = 0, len(nums)-1
         if nums[lo] < nums[hi]:
             return nums[lo]
         while lo <= hi:
             mid = lo + (hi - lo) // 2
-            # 这里需要边界检查
-            if mid < len(nums)-1 and nums[mid] > nums[mid+1]:
+            # firstly,
+            # we check if the mid and its neighbors are the inversed pair
+            if mid < len(nums)-1 and nums[mid] > nums[mid+1]:  # right neighbor
                 return nums[mid+1]
-            if nums[mid-1] > nums[mid]: # 逆序对在最前会被上面语句截断
+            if nums[mid-1] > nums[mid]:  # left neighbor
                 return nums[mid]
-            if nums[mid] > nums[lo]:
-                lo = mid + 1
+            if nums[mid] > nums[lo]: # left array is sorted
+                lo = mid + 1  # mid is not the answer, it is safe to exclude it
             elif nums[mid] < nums[lo]:
                 hi = mid - 1
-            # 全等数组lo会累加，所以也需要边界检查
+            # what if all elements are equal? we have to check boundary
             elif lo < len(nums)-1 and nums[lo] > nums[lo+1]:
                 return nums[lo+1]
             else:
@@ -45,10 +74,11 @@ class Q154(Solution):
 
 def main():
     q = Q154()
-    q.add_args([1, 3, 5])
-    q.add_args([3, 3, 3])
-    q.add_args([10, 1, 2, 3, 4, 5, 6, 7])
-    q.add_args([10, 1, 10, 10, 10])
+    q.add_case(q.case([1, 3, 5]).assert_equal(1))
+    q.add_case(q.case([3, 5, 1]).assert_equal(1))
+    q.add_case(q.case([3, 3, 3]).assert_equal(3))
+    q.add_case(q.case([10, 1, 2, 3, 4, 5, 6, 7]).assert_equal(1))
+    q.add_case(q.case([10, 1, 10, 10, 10]).assert_equal(1))
     q.run()
 
 
