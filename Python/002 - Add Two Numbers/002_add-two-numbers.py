afe714476f385668b7d28e4610683c8649b6a956
aptend <crescentwhale@hotmail.com>
2020-02-19 09:37:45 +0800

    review 2

--- Python/002 - Add Two Numbers/002_add-two-numbers.py
+++ Python/002 - Add Two Numbers/002_add-two-numbers.py
@@ -1,6 +1,7 @@
 from leezy import solution, Solution
 from leezy.assists import LinkedListContext, LinkedListNode
 
+
 class Q002(Solution):
     @solution
     def addTwoNumbers(self, l1, l2):
@@ -13,7 +14,7 @@ class Q002(Solution):
             if l1:
                 s += l1.val
                 l1 = l1.next
-            
+
             if l2:
                 s += l2.val
                 l2 = l2.next
@@ -26,16 +27,20 @@ class Q002(Solution):
         return dummy.next
 
 
-
-
 def main():
     q = Q002()
     q.set_context(LinkedListContext)
-    q.add_args([2, 4, 3], [5, 6, 4])
-    q.add_args([2, 4, 3, 1], [5, 6, 4])
-    q.add_args([5], [5])
-    q.add_args([1], [9, 9, 9, 9])
+    make = LinkedListNode.make_linked_list
+    q.add_case(q.case([2, 4, 3], [5, 6, 4])
+                .assert_equal(make([7, 0, 8])))
+    q.add_case(q.case([2, 4, 3, 1], [5, 6, 4])
+                .assert_equal(make([7, 0, 8, 1])))
+    q.add_case(q.case([5], [5])
+                .assert_equal(make([0, 1])))
+    q.add_case(q.case([1], [9, 9, 9, 9])
+                .assert_equal(make([0, 0, 0, 0, 1])))
     q.run()
+
 
 if __name__ == '__main__':
     main()
