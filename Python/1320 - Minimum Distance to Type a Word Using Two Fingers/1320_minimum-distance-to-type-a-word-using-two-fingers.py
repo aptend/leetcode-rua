607122d730ee0bc0870900841880e1a84352e26e
aptend <crescentwhale@hotmail.com>
2020-03-03 16:58:02 +0800

    1320 add dp-push solution

--- Python/1320 - Minimum Distance to Type a Word Using Two Fingers/1320_minimum-distance-to-type-a-word-using-two-fingers.py
+++ Python/1320 - Minimum Distance to Type a Word Using Two Fingers/1320_minimum-distance-to-type-a-word-using-two-fingers.py
@@ -14,6 +14,9 @@ class Q1320(Solution):
     def min_distance(self, word):
         # 520 ms, faster than 36.18%
         memo = {}
+
+        # solve(i, chx, chy): what is the min cost to type word[i..] when
+        # the left finger is on chx and the right finger is on chy
         def solve(i, chx, chy):
             if i == len(word):
                 return 0
@@ -31,6 +34,9 @@ class Q1320(Solution):
         # 256 ms, faster than 91.13%
         N = len(word)
         memo = [[None] * 27 for _ in range(N)]
+
+        # solve(i, chx): what is the min cost to type word[i..] when
+        # one finger is on word[i-1] and another is on chx(chx != word[i-1])
         def solve(i, chx):
             if i == len(word):
                 return 0
@@ -44,6 +50,25 @@ class Q1320(Solution):
             return ans
         return solve(0, 26)
 
+    @solution
+    def min_distance_dp_push(self, word):
+        # 228ms, 92.34%
+        N = len(word)
+        MAX = float('inf')
+        dp = [[MAX] * 27 for _ in range(N)]
+        dp[0][26] = 0
+        for i in range(N-1):
+            cur_ch = ord(word[i]) - ord('A')
+            nxt_ch = ord(word[i+1]) - ord('A')
+            for j in range(27):
+                if not dp[i][j] < MAX:  # we just push possible values
+                    continue
+                dp[i+1][j] = min(dp[i+1][j],
+                                 dp[i][j] + self.cost(cur_ch, nxt_ch))
+                dp[i+1][cur_ch] = min(dp[i+1][cur_ch],
+                                      dp[i][j] + self.cost(j, nxt_ch))
+        return min(dp[N-1])
+
 
 def main():
     q = Q1320()
@@ -52,6 +77,7 @@ def main():
     q.add_case(q.case('NEW').assert_equal(3))
     q.add_case(q.case('YEAR').assert_equal(7))
     q.run()
+
 
 if __name__ == '__main__':
     main()
